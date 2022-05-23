b768df6744ef08a68f17ab0caf50de46d76323ff
Cairison <cairisonds99@gmail.com>
2022-05-23 17:25:42 -0400

    versão 0.2

---
+++ quintoDia/funcao.js
@@ -0,0 +1,93 @@
+const frutas = [];
+const laticinios = [];
+const congelados = [];
+const doces = [];
+let perguntaSimNao = prompt(
+  "Você deseja adicionar um alimento a sua lista de compras? (1 - sim ou    2 - não)"
+);
+let encerrarPrograma = false;
+
+while (encerrarPrograma === false) {
+  while (perguntaSimNao === "1") {
+    var alimento = prompt("Qual alimento você deseja inserir?");
+
+    var categoria = prompt(
+      "Em qual categoria esse alimento se encaixa? ( 1 = frutas, 2 = laticinios, 3 = congelados ou 4 = doces?)"
+    );
+
+    if (categoria === "1") {
+      frutas.push(alimento);
+    } else if (categoria === "2") {
+      laticinios.push(alimento);
+    } else if (categoria === "3") {
+      congelados.push(alimento);
+    } else if (categoria === "4") {
+      doces.push(alimento);
+    }
+
+    perguntaSimNao = prompt(
+      `Você deseja adicionar um alimento a sua lista de compras? (responda com 1 para sim ou 2 para não ou 3 para remover algum elemento da lista)`
+    );
+
+    if (perguntaSimNao === "3") {
+      encerrarPrograma = true;
+    }
+
+    while (perguntaSimNao === "3") {
+      var removerItem = prompt(
+        `Você deseja remover um item de qual categoria? (1 = frutas, 2 = laticinios, 3 = congelados, 4 = doces)`
+      );
+
+      if (removerItem === "1") {
+        var removerFrutas = prompt(
+          `Os itens dessa categoria são: ${frutas}. Qual você deseja remover?`
+        );
+        var iFrutas = frutas.indexOf(removerFrutas);
+        frutas.splice(iFrutas, 1);
+        alert(`${removerFrutas} foi removido com sucesso!`);
+        encerrarPrograma = false;
+        perguntaSimNao = prompt(
+          `Você deseja adicionar um alimento a sua lista de compras? (responda com 1 para sim ou 2 para não ou 3 para remover algum elemento da lista)`
+        );
+      } else if (removerItem === "2") {
+        var removerLaticinios = prompt(
+          `Os itens dessa categoria são: ${laticinios}. Qual você deseja remover?`
+        );
+        var iLaticinios = laticinios.indexOf(removerLaticinios);
+        laticinios.splice(iLaticinios, 1);
+        alert(`${removerLaticinios} foi removido com sucesso!`);
+        perguntaSimNao = prompt(
+          `Você deseja adicionar um alimento a sua lista de compras? (responda com 1 para sim ou 2 para não ou 3 para remover algum elemento da lista)`
+        );
+        encerrarPrograma = false;
+      } else if (removerItem === "3") {
+        var removerCongelados = prompt(
+          `Os itens dessa categoria são: ${congelados}. Qual você deseja remover?`
+        );
+        var iCongelados = congelados.indexOf(removerCongelados);
+        congelados.splice(iCongelados, 1);
+        alert(`${removerCongelados} foi removido com sucesso!`);
+        perguntaSimNao = prompt(
+          `Você deseja adicionar um alimento a sua lista de compras? (responda com 1 para sim ou 2 para não ou 3 para remover algum elemento da lista)`
+        );
+        encerrarPrograma = false;
+      } else if (removerItem === "4") {
+        var removerDoces = prompt(
+          `Os itens dessa categoria são: ${doces}. Qual você deseja remover?`
+        );
+        var iDoces = doces.indexOf(removerDoces);
+        doces.splice(iDoces, 1);
+        alert(`${removerDoces} foi removido com sucesso!`);
+        perguntaSimNao = prompt(
+          `Você deseja adicionar um alimento a sua lista de compras? (responda com 1 para sim ou 2 para não ou 3 para remover algum elemento da lista)`
+        );
+        encerrarPrograma = false;
+      }
+    }
+  }
+  if (perguntaSimNao === "2") {
+    document.getElementById
+      alert(`Lista de compras:\n  Frutas: ${frutas}\n  Laticínios: ${laticinios}\n  Doces: ${doces}\n  Congelados: ${congelados}`);
+      encerrarPrograma = true;
+  }
+}
---
+++ setimoDia/funcao.js
@@ -0,0 +1,21 @@
+function calcular(){
+    let val1   = parseInt(document.getElementById("val1").value);
+    let val2   = parseInt(document.getElementById("val2").value);
+    let sinal  = document.getElementById("sinal").value;
+    let result = document.getElementById("result");
+
+    if(sinal == "+"){
+        result.value = val1 + val2
+    }else if(sinal == "-"){
+        result.value = val1 - val2
+    }else if(sinal == "*"){
+        result.value = val1 * val2
+    }else if(sinal == "/"){
+        result.value = val1 / val2
+    }
+}
+
+function resetar() {
+    document.getElementById("val1").value = "";
+    document.getElementById("val2").value = "";
+  }
